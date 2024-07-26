5b775ccc451e9c607dee3d87776c328dbcbc426b
George Langham <george.langham@autodesk.com>
2024-07-26 20:47:26 +0100

    create ButtonWordInput

---
+++ src/components/ButtonWordsInput.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { BlackoutWord } from '@/types';
+
+const emit = defineEmits(['removeWord']);
+
+const props = defineProps<{ wordsArray: BlackoutWord[] }>();
+</script>
+
+<template>
+    <h2>Click words to remove</h2>
+    <ul class="unstyled-list">
+        <li v-for="word in props.wordsArray" :key="word.id">
+            <button @click="emit('removeWord', word.id)">{{ word.label }}</button>
+        </li>
+    </ul>
+</template>
+
