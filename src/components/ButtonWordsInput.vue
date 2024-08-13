97f06304eef922435d6dfb2f3de36bb4396e5795
GeorgeWL <georgelangham1990@gmail.com>
2024-08-13 03:02:25 +0100

    add style conditional, move header

--- src/components/ButtonWordsInput.vue
+++ src/components/ButtonWordsInput.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import { defineProps, defineEmits } from 'vue';
-import type { BlackoutWord } from '@/types';
+import { defineProps, defineEmits } from 'vue'
+import type { BlackoutWord } from '@/types'
 
-const emit = defineEmits(['removeWord']);
+const emit = defineEmits(['removeWord'])
 
 const props = defineProps<{
-  wordsArray: BlackoutWord[];
-}>();
+  wordsArray: BlackoutWord[]
+}>()
 </script>
 
 <template>
-    <h2>Click words to remove</h2>
-    <ul class="unstyled-list">
-        <li v-for="word in props.wordsArray" :key="word.id">
-            <button @click="emit('removeWord', word.id)">{{ word.label }}</button>
-        </li>
-    </ul>
+  <ul class="unstyled-list">
+    <li v-for="word in props.wordsArray" :key="word.id">
+      <button :class="{ blackout: word.disabled }" @click="emit('removeWord', word.id)">
+        {{ word.label }}
+      </button>
+    </li>
+  </ul>
 </template>
-
