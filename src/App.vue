cca788fe46f5045f6b4e935862baf7cf2675dfb1
GeorgeWL <georgelangham1990@gmail.com>
2024-08-14 02:54:20 +0100

    remove unecessary style,s make column

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import type { BlackoutWord } from '@/types'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import ButtonWordsInput from './components/ButtonWordsInput.vue'
 import PageHeader from './components/PageHeader.vue'
-const defaultText =
-  'Hello, world! This is a test. It includes a few words. Some repeated words are test, test, test.'
-const wordsArray = ref<BlackoutWord[]>(
-  defaultText.split(' ').map((word, index) => ({ id: index.toString(), label: word }))
+import { mainStore } from './stores/mainStore'
+import InputSelector from './components/InputSelector.vue'
+
+const text = mainStore.text
+const wordsArray = computed<BlackoutWord[]>(() =>
+  text.split(' ').map((word, index) => ({ id: index.toString(), label: word }))
 )
+const arrayRef = ref(wordsArray.value)
 const markWordAsBlackout = (id: string) => {
-  const words = wordsArray.value
+  const words = arrayRef.value
   words.find((word) => word.id === id)!.disabled = !words.find((word) => word.id === id)!.disabled
-  wordsArray.value = words
+  arrayRef.value = words
 }
 </script>
 
@@ -21,55 +24,48 @@ const markWordAsBlackout = (id: string) => {
   </header>
   <main>
     <section>
+      <InputSelector />
+    </section>
+    <section>
       <h2>Click words to add/remove</h2>
-      <ButtonWordsInput @remove-word="markWordAsBlackout" :words-array="wordsArray" />
+      <ButtonWordsInput @remove-word="markWordAsBlackout" :words-array="arrayRef" />
     </section>
   </main>
 </template>
 
-<style scoped>
+<style>
 header {
   line-height: 1.5;
 }
 
 main {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   place-items: flex-start;
   align-items: center;
   text-align: left;
+  height: 90vh;
   @media screen and (max-width: 950px) {
     flex-direction: column;
   }
 }
 
 section {
-  display: grid;
-  gap: var(--column-gap);
-  grid-template-columns: 1fr;
-  grid-template-rows: 0.5fr 1fr;
-  padding-left: var(--column-gap);
+  display: flex;
+  flex-direction: column;
+  place-items: flex-start;
+  padding: var(--section-gap) 0;
   width: 100%;
-  @media screen and (max-width: 950px) {
-    padding-top: var(--column-gap);
-    padding-left: 0;
-  }
+  border-top: var(--color-border) 1px solid;
+  align-items: flex-start;
 }
 
-section:first-child {
-  padding-right: var(--column-gap);
-  border-right: var(--color-border) 1px solid;
-  @media screen and (max-width: 950px) {
-    border-right: none;
-    padding-right: inherit;
-  }
-}
 
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    padding-bottom: calc(var(--section-gap) / 2);
   }
 
   header .wrapper {
