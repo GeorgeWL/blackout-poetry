73cbc1bf617981c5ede3aaf3d26d6c23c62c5a99
GeorgeWL <georgelangham1990@gmail.com>
2024-08-14 02:54:37 +0100

    make use of store

---
+++ src/stores/mainStore.ts
@@ -0,0 +1,14 @@
+import { reactive } from 'vue'
+
+const defaultText =
+  'Hello, world! This is a test. It includes a few words. Some repeated words are test, test, test.'
+
+export const mainStore = reactive({
+  text: defaultText,
+  changeText(newText: string) {
+    this.text = newText
+  },
+  clearText() {
+    this.text = ''
+  }
+})
