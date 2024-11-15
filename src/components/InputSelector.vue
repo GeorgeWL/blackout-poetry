16a9ec7b0f915f4c41a2d791b907f8f775c8c537
GeorgeWL <georgelangham1990@gmail.com>
2024-11-15 22:35:58 +0000

    add option to hide search results

--- src/components/InputSelector.vue
+++ src/components/InputSelector.vue
@@ -26,9 +26,9 @@ const resetText = () => {
     <BookSearch />
   </div>
   <div v-if="inputType === 'manual'" class="flex-col">
-    <textarea placeholder="Type your text here" v-model="cacheText" cols="100" rows="10"></textarea>
+    <textarea for="submit-manual" placeholder="Type your text here" v-model="cacheText" cols="100" rows="10"></textarea>
     <div class="flex flex-row">
-      <button type="submit" @click="saveText">Save</button>
+      <button type="submit" id="submit-manual" @click="saveText">Save</button>
       <button type="reset" @click="cacheText = ''">Clear</button>
       <button type="reset" @click="resetText">Reset to Default</button>
     </div>
