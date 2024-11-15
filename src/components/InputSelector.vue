9a09610251b4f4973d69f8a71fa4db764dabc9be
GeorgeWL <georgelangham1990@gmail.com>
2024-11-15 20:09:07 +0000

    add reset test

--- src/components/InputSelector.vue
+++ src/components/InputSelector.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
-import { mainStore } from '@/stores/mainStore'
+import { defaultText, mainStore } from '@/stores/mainStore'
 import { ref } from 'vue'
 import BookSearch from './BookSearch.vue'
-
-const cachedText = ref(mainStore.text)
 const inputType = ref<'search' | 'manual'>('manual')
+const cacheText = ref(mainStore.text)
+const saveText = () => {
+  mainStore.setText(cacheText.value)
+}
+const resetText = () => {
+  cacheText.value = defaultText;
+  mainStore.setText(defaultText)
+}
 </script>
 
 <template>
   <div class="">
     <input type="radio" v-model="inputType" id="search-text" value="search" />
-    <label for="search-text">Search</label>
+    <label for="search-text">Search*</label>
   </div>
   <div>
     <input type="radio" v-model="inputType" id="manual-text" value="manual" />
@@ -20,10 +26,11 @@ const inputType = ref<'search' | 'manual'>('manual')
     <BookSearch />
   </div>
   <div v-if="inputType === 'manual'" class="flex-col">
-    <textarea placeholder="Type your text here" v-model="cachedText"></textarea>
+    <textarea placeholder="Type your text here" v-model="cacheText" cols="100" rows="10"></textarea>
     <div class="flex flex-row">
-      <button @click="mainStore.changeText(cachedText.valueOf())" class="btn">Save</button>
-      <button @click="cachedText=''">Clear</button>
+      <button type="submit" @click="saveText">Save</button>
+      <button type="reset" @click="cacheText = ''">Clear</button>
+      <button type="reset" @click="resetText">Reset</button>
     </div>
   </div>
 </template>
