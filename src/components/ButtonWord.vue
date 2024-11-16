6bf1327d9e09fe2db2e75e61bb3b16b853f5cc98
GeorgeWL <georgelangham1990@gmail.com>
2024-11-16 00:08:17 +0000

    mouseout

--- src/components/ButtonWord.vue
+++ src/components/ButtonWord.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import type { BlackoutWord } from '@/types'
+import { ref } from 'vue';
 
 const props = defineProps<{
   word: BlackoutWord
   toggleWord: (id: string) => void
+  mouseout?: boolean
 }>()
 </script>
 <template>
-  <button v-if="!props.word.isNewLine" :class="{ blackout: props.word.disabled }" @click="toggleWord(props.word.id)">
+  <button v-if="!props.word.isNewLine" :class="{ blackout: props.word.disabled }" v-on:mouseout="()=>{if(props.mouseout)toggleWord(props.word.id)}" @click="toggleWord(props.word.id)">
     {{ props.word.label }}
   </button>
   <div v-else class="spacer" />
--- src/components/ButtonWordsInput.vue
+++ src/components/ButtonWordsInput.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
-import { defineProps, defineEmits } from 'vue'
+import { defineProps, defineEmits, ref } from 'vue'
 import type { BlackoutWord } from '@/types'
 import ButtonWord from './ButtonWord.vue'
 
 const emit = defineEmits(['toggleWord'])
 
+const mouseout = ref(false)
 const props = defineProps<{
   wordsArray: BlackoutWord[]
 }>()
 </script>
-@click=""
 
 <template>
+  <div>
+    <input type="checkbox" v-model="mouseout" id="toggle-mouseout" />
+    <label for="toggle-mouseout">Toggle Mouseout</label>
+  </div>
   <ul class="unstyled-list">
     <li v-for="word in props.wordsArray" :key="word.id">
-      <ButtonWord :word="word" :toggleWord="()=>emit('toggleWord', word.id)" />
+      <ButtonWord :word="word" :toggleWord="()=>emit('toggleWord', word.id)" :mouseout="mouseout"/>
     </li>
   </ul>
 </template>
